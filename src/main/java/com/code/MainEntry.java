5a95d51aaee94cbc24558b383845a79d876ad285
shepard <shepard.xia@hotamil.com>
2020-03-15 12:45:01 +0800

    修改实现方式

--- src/main/java/com/code/MainEntry.java
+++ src/main/java/com/code/MainEntry.java
@@ -4,6 +4,10 @@ import com.code.test.UserInput;
 
 import java.util.Scanner;
 
+
+/**
+ * 控制台输入数字，返回结果
+ * */
 public class MainEntry {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
